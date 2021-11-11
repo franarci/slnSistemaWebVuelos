a1ba5110786d103d239fd07fb7ed9ac1d542a3e9
franarci <fran.arciniega96@gmail.com>
2021-11-10 21:31:36 -0300

    Agrego BeforeAfterFilter en HomeController

--- SistemaWebVuelos/Controllers/HomeController.cs
+++ SistemaWebVuelos/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SistemaWebVuelos.Filter;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,6 +7,7 @@ using System.Web.Mvc;
 
 namespace SistemaWebVuelos.Controllers
 {
+    [BeforeAfterFilter]
     public class HomeController : Controller
     {
         // GET: Home
