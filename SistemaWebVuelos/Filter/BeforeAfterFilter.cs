9aa0703d2927a0f9a563d35f18755bf5485a5442
franarci <fran.arciniega96@gmail.com>
2021-11-10 21:32:32 -0300

    BeforeAfterFilter

---
+++ SistemaWebVuelos/Filter/BeforeAfterFilter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SistemaWebVuelos.Filter
+{
+    public class BeforeAfterFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var controller = filterContext.RouteData.Values["controller"];
+            var action = filterContext.RouteData.Values["action"];
+            Debug.WriteLine(controller + " Action: " + action + " Paso por OnActionExecuting");
+        }
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            //NO COPIAR Y PEGAR ESTE METODO, ESCRIBIRLO A MANO
+            var controller = filterContext.RouteData.Values["controller"];
+            var action = filterContext.RouteData.Values["action"];
+            Debug.WriteLine(controller + " Action: " + action + " Paso por OnActionExecuting");
+            base.OnActionExecuted(filterContext);
+        }
+    }
+}
